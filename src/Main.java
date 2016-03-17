478f1e0f2dc47f60db8b73fd6c7f3b54a436392b
Fabrice Servais <fabrice.servais@gmail.com>
2016-03-17 23:30:09 +0100

    Retrieve tweets with limit in number

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,84 @@
 import twitter4j.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+        List<Status> tweets = null;
+
+        tweets = searchTweet("#AppleVSFBI", 5000);
+
+        System.out.println("Number of tweets: " + tweets.size());
+
+    }
+
+    /**
+     * Search the tweet corresponding to a query.
+     * @param query Query to search.
+     * @param maxNumberTweets Maximum number of tweets to retrieve. -1 if no limit (actually will be set to to max 180 requests).
+     * @return List of tweet matching the query.
+     * @throws TwitterException
+     */
+    private static List<Status> searchTweet(String query, int maxNumberTweets) {
+        System.out.print("Retrieving page ");
+        int pageNumber = 1; // For log purpose
+
         Twitter twitter = TwitterFactory.getSingleton();
 
-        Query query = new Query("#AppleVsFBI");
-        query.setCount(200);
-        query.setLang("en");
-        query.setResultType(Query.ResultType.recent);
+        Query twitterQuery = new Query(query);
+        twitterQuery.setCount(100); // Max # of tweet per page (100 is max)
+        twitterQuery.setLang("en");
+        twitterQuery.setResultType(Query.ResultType.mixed);
 
-        try {
-            QueryResult results = twitter.search(query);
-            List<Status> tweets = results.getTweets();
+        List<Status> tweets = new ArrayList<>();
 
-            for(Status tweet : tweets){
-                System.out.println("@" + tweet.getUser().getScreenName() + " : " + tweet.getText() + "\n");
+        if(maxNumberTweets == -1)
+            maxNumberTweets = Integer.MAX_VALUE;
+
+        long maxId = Long.MAX_VALUE;
+
+        QueryResult results;
+        do {
+            // Log
+            System.out.print(pageNumber + "...");
+            ++pageNumber;
+            if(pageNumber % 10 == 0)
+                System.out.println();
+
+            // Maximum number of requests by Twitter API (every 15 minutes)
+            if(pageNumber >= 180)
+                break;
+
+            try {
+                results = twitter.search(twitterQuery);
+            } catch (TwitterException e) {
+                System.out.println();
+                return tweets;
             }
 
-            System.out.println("Received " + tweets.size() + " tweets");
-        } catch (TwitterException e) {
-            e.printStackTrace();
-        }
+            List<Status> resultsTweets = results.getTweets();
+
+            for(Status tweet : resultsTweets){
+                if(tweet.getId() < maxId)
+                    maxId = tweet.getId();
+                
+                // Ignore retweet since the original tweet should be in the search since it should also match the query
+                if(tweet.isRetweet())
+                    continue;
+
+                tweets.add(tweet);
+
+                if(tweets.size() >= maxNumberTweets)
+                    break;
+            }
+
+            twitterQuery.setMaxId(maxId);
+
+        } while(tweets.size() < maxNumberTweets && results.getTweets().size() > 0);
+        System.out.println();
+
+        return tweets;
     }
 }
