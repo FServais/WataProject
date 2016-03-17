ee41c211b7d126b630b8205ca737c04fd7172b7f
Fabrice Servais <fabrice.servais@gmail.com>
2016-03-17 22:49:12 +0100

    First retrieve of tweets

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import twitter4j.*;
+
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Twitter twitter = TwitterFactory.getSingleton();
+
+        Query query = new Query("#AppleVsFBI");
+        query.setCount(200);
+        query.setLang("en");
+        query.setResultType(Query.ResultType.recent);
+
+        try {
+            QueryResult results = twitter.search(query);
+            List<Status> tweets = results.getTweets();
+
+            for(Status tweet : tweets){
+                System.out.println("@" + tweet.getUser().getScreenName() + " : " + tweet.getText() + "\n");
+            }
+
+            System.out.println("Received " + tweets.size() + " tweets");
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
     }
 }
