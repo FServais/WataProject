63f497f1679777ac0930407cdcf2bda9ac5ade61
Michele Imperiali <mjimperiali@student.ulg.ac.be>
2016-04-09 11:31:48 +0200

    Setup dependecies

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         List<Status> tweets = null;
 
-        tweets = searchTweet("#AppleVSFBI", 5000);
+        tweets = searchTweet("#AppleVSFBI", 10);
 
         System.out.println("Number of tweets: " + tweets.size());
 
