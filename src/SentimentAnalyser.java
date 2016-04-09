6ff207b5f3dec1c4eaadffa8b2b7ef7bb5576bfa
Fabrice Servais <fabrice.servais@student.ulg.ac.be>
2016-04-09 18:47:56 +0200

    Ignore class which is not 0 or 1

--- src/SentimentAnalyser.java
+++ src/SentimentAnalyser.java
@@ -13,8 +13,8 @@ import java.util.*;
 public class SentimentAnalyser {
 
     final String DICTIONARY = "sentiment-dict.txt";
-    final String HASHTAG = "#AppleVsFBI";
-    final int MAX_TWEETS = 3;
+    final String HASHTAG = "#BatmanVsSuperman";
+    final int MAX_TWEETS = 100;
     final int UPPER_BOUND = 2;
     final int LOWER_BOUND = -2;
     final String TRAINING_DATA_FILE = "training_data_file.csv";
@@ -42,6 +42,8 @@ public class SentimentAnalyser {
                 int score = score(tweet);
                 scoreMap.put(tweet, score);
                 int tweetClass = getTweetClass(tweet, score);
+                if (tweetClass != 0 && tweetClass != 1)
+                    continue;
                 writer.write(tweet + "," + tweetClass + "\n");
             }
             writer.close();
@@ -82,33 +84,33 @@ public class SentimentAnalyser {
             if (this.sentimentWords.containsKey(word)){//first we check if the word is contained in ou dictionary
                 finalScore += this.sentimentWords.get(word);
             }
-            else{//if not we check through a Python program what's the lemma of the word
-                int weight = 0;
-                try {
-                    Process p = Runtime.getRuntime().exec("python3 lemmatizer.py " + word);
-                    BufferedReader stdInput = new BufferedReader(new
-                            InputStreamReader(p.getInputStream()));
-
-                    BufferedReader stdError = new BufferedReader(new
-                            InputStreamReader(p.getErrorStream()));
-
-                    // read the output from the command
-                    String output = stdInput.readLine();
-
-                    if(output.length()>0){
-                        //first we check that the python script was able to identify the meaning of the abbreviation
-                        //this means that output has to be different from word
-                        if(output.equals(word)){
-                            //the script didn't resolve the word, so we have to add it to the dictionary
-                            weight = DictionnaryParser.addWordToMap(word);
-                            sentimentWords.put(word, weight);
-                        }
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
+//            else{//if not we check through a Python program what's the lemma of the word
+//                int weight = 0;
+//                try {
+//                    Process p = Runtime.getRuntime().exec("python3 lemmatizer.py " + word);
+//                    BufferedReader stdInput = new BufferedReader(new
+//                            InputStreamReader(p.getInputStream()));
+//
+//                    BufferedReader stdError = new BufferedReader(new
+//                            InputStreamReader(p.getErrorStream()));
+//
+//                    // read the output from the command
+//                    String output = stdInput.readLine();
+//
+//                    if(output.length()>0){
+//                        //first we check that the python script was able to identify the meaning of the abbreviation
+//                        //this means that output has to be different from word
+//                        if(output.equals(word)){
+//                            //the script didn't resolve the word, so we have to add it to the dictionary
+//                            weight = DictionnaryParser.addWordToMap(word);
+//                            sentimentWords.put(word, weight);
+//                        }
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
         }
 
         return finalScore;
@@ -121,7 +123,7 @@ public class SentimentAnalyser {
             return 1;
         else{
             Scanner reader = new Scanner(System.in);  // Reading from System.in
-            System.out.println("Is this tweet positive (0) or negative (1): " + tweet);
+            System.out.println("Is this tweet positive (0) or negative (1) (or ignore (2)): " + tweet);
             return reader.nextInt();
         }
     }
