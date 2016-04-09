250e061efff0f305b54594e682ac393bb0728443
laurent.vanosmael@student.ulg.ac.be <laurent.vanosmael@student.ulg.ac.be>
2016-04-09 15:30:04 +0200

    main test

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,29 @@
 import twitter4j.*;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        TwitterSearchEngine searchEngine = new TwitterSearchEngine();
-
-        List<Status> tweets = null;
-//        long lastId = -1;
+    	TwitterSearchEngine searchEngine = new TwitterSearchEngine();
+    	TweetCleaner tc = new TweetCleaner();
+    	
+        List<Status> tweets = null;      
+        ArrayList<String> cleans = null;
         long lastId = 717428853405327365L;
-        tweets = searchEngine.searchTweet("#AppleVsFBI", 2, lastId);
-
+        tweets = searchEngine.searchTweet("#AppleVsFBI", 10, lastId);
+        cleans = tc.cleanTweets(tweets);
+        
         System.out.println("Number of tweets: " + tweets.size());
-        for(Status tweet : tweets){
-            System.out.println(tweet.getText());
+        for(int i = 0; i < tweets.size(); ++i){
+        	System.out.println("NEXT TWEET : ");
+            System.out.println(tweets.get(i).getText());
+            System.out.println("Clean : ");
+            System.out.println(cleans.get(i));
+            
         }
+
     }
 }
